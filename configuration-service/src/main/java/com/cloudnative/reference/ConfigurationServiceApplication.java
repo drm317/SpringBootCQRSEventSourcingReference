e92acb3df4a8633c12b2e83a10fa8325b348b346
Daniel Marlow <drm3107@gmail.com>
2017-09-23 10:58:16 +0100

    Configuration service

---
+++ configuration-service/src/main/java/com/cloudnative/reference/ConfigurationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cloudnative.reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+}
