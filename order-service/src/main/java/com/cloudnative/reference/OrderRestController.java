5738ececf9d1f23669560543fbc8eff977568830
Daniel Marlow <drm3107@gmail.com>
2017-09-25 21:04:02 +0100

    fixed order service configuration

--- order-service/src/main/java/com/cloudnative/reference/OrderRestController.java
+++ order-service/src/main/java/com/cloudnative/reference/OrderRestController.java
@@ -2,6 +2,7 @@ package com.cloudnative.reference;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.net.URI;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -13,25 +14,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/order", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 public class OrderRestController {
 
-	private final CommandGateway cg;
+	private final CommandGateway commandGateway;
 
 	@Autowired
 	OrderRestController(CommandGateway cg) {
-		this.cg = cg;
+		this.commandGateway = cg;
 	}
 
 	@PostMapping
 	CompletableFuture<ResponseEntity<?>> createOrder(@RequestBody Map<String, String> body) {
 
 		String id = UUID.randomUUID().toString();
-		CreateOrderCommand order = new CreateOrderCommand(id, body.get("description"));
-
+		CreateOrderCommand orderCommand = new CreateOrderCommand(id, body.get("description"));
 		return null;
+	}
+
+	private static URI uri(String uri, Map<String, String> template) {
+		UriComponents uriComponents = UriComponentsBuilder.newInstance().path(uri).build().expand(template);
+		return uriComponents.toUri();
 	}
 
 }
